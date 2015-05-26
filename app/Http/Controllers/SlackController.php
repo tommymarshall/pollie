e86ae364826a172598e3f34d49205c1031330050
Tommy Marshall <tommy.marshall@viget.com>
2015-05-26 19:37:36 -0400

    Use test config if testing, convert array to string

--- app/Http/Controllers/SlackController.php
+++ app/Http/Controllers/SlackController.php
@@ -22,17 +22,23 @@ class SlackController extends Controller {
 
     public function listen(Request $request)
     {
-        $this->incoming = $this->slack->listen($request->all());
-        // $this->incoming = $this->slack->listen([
-        //     'token'        => '1wjXO8lq4Mb4wAV9QrRDCwQZ',
-        //     'team_id'      => 'T0001',
-        //     'channel_id'   => 'C2147483705',
-        //     'channel_name' => 'test',
-        //     'timestamp'    => '1355517523.000005',
-        //     'user_id'      => 'U2147483697',
-        //     'user_name'    => 'Steve',
-        //     'text'         => 'pollie: vote 2'
-        // ]);
+        if (getenv('APP_ENV') === 'local')
+        {
+            $this->incoming = $this->slack->listen([
+                'token'        => '1wjXO8lq4Mb4wAV9QrRDCwQZ',
+                'team_id'      => 'T0001',
+                'channel_id'   => 'C2147483705',
+                'channel_name' => 'test',
+                'timestamp'    => '1355517523.000005',
+                'user_id'      => 'U2147483697',
+                'user_name'    => 'Steve',
+                'text'         => 'pollie: vote 2'
+            ]);
+        }
+        else
+        {
+            $this->incoming = $this->slack->listen($request->all());
+        }
 
         $command = $this->getCommand($this->incoming->words());
 
